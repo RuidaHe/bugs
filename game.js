f81da54115e368aa8fd4361eb9cec2bd5b9246fa
Ruida He <radarhe1990@gmail.com>
2015-10-10 23:18:01 -0400

    Add updateTime()

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
+
+
 function startgame(){
-	document.getElementById("startpage").style.display = "none";
+	time = 60;
+	
+	startpage = document.getElementById("startpage");
+	startpage.style.display = "none";
 	
 	infobar = document.createElement("div");
 	infobar.id="infobar";
@@ -19,5 +24,33 @@ function startgame(){
 	score.innerHTML = "Score: 0";
 	score.id = "score";
 	infobar.appendChild(score);
+
+	viewport = document.createElement("canvas");
+	viewport.id = "viewport";
+	document.body.appendChild(viewport);
+
+	 times = setInterval(updateTime, 20);
 	
+	
+}
+
+function updateTime(){
+	if(time>0){
+		time--;
+		timer.innerHTML = time + "sec";
+	}
+	else{
+		if(confirm ("Try again!")){			
+			time=60;
+		}
+		else {
+			time=60;
+			clearInterval(times);
+			infobar.style.display = "none";
+			viewport.style.display = "none";
+			startpage.style.display = "block";
+
+		}
+		
+	}
 }
