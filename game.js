268b07a7c5e9186ecfa25cbccf6400e9506142c5
Ruida He <radarhe1990@gmail.com>
2015-10-11 23:17:18 -0400

    Add food

--- game.js
+++ game.js
@@ -4,6 +4,13 @@ function startgame(){
 	time = 60;
 	frame = 0;
 	state = true;
+	foodlist = new Array();
+	foodlist[0] = new food((Math.random()*300), (Math.random()*280));
+	foodlist[1] = new food((Math.random()*300), (Math.random()*280));
+	foodlist[2] = new food((Math.random()*300), (Math.random()*280));
+	foodlist[3] = new food((Math.random()*300), (Math.random()*280));
+	foodlist[4] = new food((Math.random()*300), (Math.random()*280));
+
 	startpage = document.getElementById("startpage");
 	startpage.style.display = "none";
 	
@@ -30,14 +37,25 @@ function startgame(){
 
 	viewport = document.createElement("canvas");
 	viewport.id = "viewport";
+	ctx = viewport.getContext("2d");
 	document.body.appendChild(viewport);
 
+	
 	times = setInterval(updateTime, 20);
 	
 	
 }
 
+// function showCoords(event) {
+	
+//     var x = event.clientX;
+//     var y = event.clientY;
+//     var coords = "X coords: " + x + ", Y coords: " + y;
+//     alert(coords);
+// }
+
 function updateTime(){
+	drawFood();
 	frame++;
 	if(frame%10 == 0 && time>0){
 		time--; 
@@ -71,5 +89,23 @@ function gamePlayPause(){
 		pause.innerHTML = "pause";
 		times = setInterval(updateTime, 20);
 
+	}
+}
+
+function food(x, y){
+	this.x = x;
+	this.y = y;
+}
+
+function drawFood(){
+	var foodImg = new Image();
+	foodImg.src = "http://pngimg.com/upload/apple_PNG4731.png";
+	var i = 0;
+	alert("a");
+	while(foodlist[i]){
+			alert("a");
+
+		ctx.drawImage(foodImg, foodlist[i].x, foodlist[i].y, 10, 10);
+		i++;
 	}
 }
